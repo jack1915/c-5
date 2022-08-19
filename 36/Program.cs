bf11786b519512d88887de8b5ad507bba59488b4
Jack <pochta1915@mail.ru>
2022-08-19 10:16:11 +0500

    create 36

---
+++ 36/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = new int[11];
+int summ = 0;
+void Array(int[] array, int min, int max){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array){
+    for (int i = 0; i<array.Length; i++ )
+  {
+    Console.Write(( i ) + " ");
+    Console.WriteLine(array[i] + " ");
+    
+  }
+  Console.WriteLine();
+}
+
+int QuantityPositive(int[] array){
+   
+    for (int i = 1; i<array.Length; )
+  {
+    summ = summ + array[i];
+    i = i + 2;
+  }
+  return summ;
+}
+Array(numbers, 100, 1000);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"(Сумма нечётных элементов равна:){summ}");
