6bd40e836f6028c7a2633815e6a8aca879d2b4d7
Jack <pochta1915@mail.ru>
2022-09-04 15:32:07 +0500

    create 54

--- 54/Program.cs
+++ 54/Program.cs
@@ -8,3 +8,59 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+Random rmd = new Random();
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Задан массив:");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+        {
+            if (numbers[i, k] < numbers[i, k + 1]) 
+            {
+                int temp = 0;
+                temp = numbers[i, k];
+                numbers[i, k] = numbers[i, k + 1];
+                numbers[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Получаем массив:");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rmd.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
