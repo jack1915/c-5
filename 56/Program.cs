c3c75bd4184e5a437d5857493cf79a71c86225de
Jack <pochta1915@mail.ru>
2022-09-04 18:48:37 +0500

    create 56

--- 56/Program.cs
+++ 56/Program.cs
@@ -7,3 +7,64 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
 // наименьшей суммой элементов: 1 строка
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+Random rmd = new Random();
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = Sum(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = Sum(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой  элементов ");
+
+int Sum(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rmd.Next(10);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
